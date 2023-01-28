3b7419f837467b3e0b3837b7441bea085b3b80c5
Dipakkamble <kambledipak700@gmail.com>
2023-01-28 20:48:20 +0530

    [Dipak] modified sum of harmonic number

---
+++ BasicCorePrograms/HarmonicNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class HarmonicNumber
+    {
+        public static void FindHarmonicNumber()
+        {
+            double sum = 0,result = 0; 
+            Console.WriteLine("Enter the nth harmonic number");
+            int N=Convert.ToInt32(Console.ReadLine());
+            for(double i=1;i<=N;i++) 
+            {
+                sum = sum + (1 / i);
+                result=sum;
+            }
+            Console.WriteLine("The sum of nth harmonic is :" + result);
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -14,7 +14,7 @@ namespace BasicCorePrograms
             Console.WriteLine("Welcome to C# Basic Programs");
 
             Console.WriteLine("please enter below number");
-            Console.WriteLine("1:FlipCoin\n2:LeapYear\n3:PowerOfTwo");
+            Console.WriteLine("1:FlipCoin\n2:LeapYear\n3:PowerOfTwo\n4:Harmonic");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -27,6 +27,9 @@ namespace BasicCorePrograms
                     break;
                  case 3:
                     PowerOfTwo.FindPowerOfTwo();
+                    break;
+                case 4:
+                    HarmonicNumber.FindHarmonicNumber();
                     break;
                 default:
                     Console.WriteLine("Please enter the option from above");
