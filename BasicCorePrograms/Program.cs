041f2973f82c6c5bde22ea51a44467513ae12b9c
Dipakkamble <kambledipak700@gmail.com>
2023-01-28 15:09:27 +0530

    [Dipak] modified flip coin percentage

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,22 @@ namespace BasicCorePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to C# Basic Programs");
+
+            Console.WriteLine("please enter below number");
+            Console.WriteLine("1:FlipCoin\n2:Exit");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    FlipCoin.FindPercentageFlipCoin();
+                    break;
+                case 2:
+                    break;
+                default:
+                    Console.WriteLine("Please enter the option from above");
+                    break;
+            }
             Console.ReadLine();
         }
     }
