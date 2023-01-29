0e27afbaf5675fc0433cb06552ec60d3364dba8f
Dipakkamble <kambledipak700@gmail.com>
2023-01-29 23:06:26 +0530

    [Dipak] modified Even and odd

---
+++ BasicCorePrograms/EvenOdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class EvenOdd
+    {
+        public static void FindEvenOdd() 
+        {
+            Console.WriteLine("Enter the number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if(num%2==0) 
+            {
+                Console.WriteLine("The number is even");
+            }
+            else 
+            {
+                Console.WriteLine("The number is odd");
+            }
+        }
+    }
+}
